d930d19dd60a7beca4f17f98961de54bc69bfe76
Viktor Ivanov <viktorivanov.dev@proton.me>
2026-09-25 04:30:22 +0200

    removed limit cuz buggy

--- scripts/viewport_camera.gd
+++ scripts/viewport_camera.gd
@@ -1,4 +1,5 @@
 extends Camera2D
+
 enum CameraMotion {LEFT, RIGHT, UP, DOWN, STATIC}
 
 @onready var timer: Timer = $Timer
@@ -10,7 +11,7 @@ enum CameraMotion {LEFT, RIGHT, UP, DOWN, STATIC}
 @export var min_zoom:float = 0.1;
 
 var speed:float;
-var can_move:bool = true;
+var can_alter_camera:bool = true;
 var is_moving:bool = false;
 var motion_status:CameraMotion = CameraMotion.STATIC;
 
@@ -19,20 +20,19 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if can_move:
-		
+	if can_alter_camera:
 		# -- Camera Movement --
 		if not speed:
 			return
-		elif Input.is_action_pressed("camera_left"):
+		if Input.is_action_pressed("camera_left"):
 			move_camera(CameraMotion.LEFT, delta)
 		elif Input.is_action_pressed("camera_right"):
 			move_camera(CameraMotion.RIGHT, delta)
-		elif Input.is_action_pressed("camera_up"):
+		if Input.is_action_pressed("camera_up"):
 			move_camera(CameraMotion.UP, delta)
 		elif Input.is_action_pressed("camera_down"):
 			move_camera(CameraMotion.DOWN, delta)
-		else:
+		if !Input.is_anything_pressed():
 			if is_moving:
 				timer.start()
 				is_moving = false;
@@ -44,14 +44,14 @@ func _process(delta: float) -> void:
 		elif Input.is_action_just_pressed("camera_zoom_in"):
 			if zoom == Vector2(max_zoom, max_zoom):
 				return;
-			if zoom + Vector2(zoom_strength, zoom_strength) > Vector2(max_zoom, max_zoom):
+			elif zoom + Vector2(zoom_strength, zoom_strength) > Vector2(max_zoom, max_zoom):
 				zoom = Vector2(max_zoom, max_zoom)
 			else:
 				zoom += Vector2(zoom_strength, zoom_strength);
 		elif Input.is_action_just_pressed("camera_zoom_out"):
 			if zoom == Vector2(min_zoom, min_zoom):
 				return;
-			if zoom - Vector2(zoom_strength, zoom_strength) < Vector2(min_zoom, min_zoom):
+			elif zoom - Vector2(zoom_strength, zoom_strength) < Vector2(min_zoom, min_zoom):
 				zoom = Vector2(min_zoom, min_zoom)
 			else:
 				zoom -= Vector2(zoom_strength, zoom_strength);
@@ -71,19 +71,19 @@ func move_camera(motion:CameraMotion, delta: float):
 		CameraMotion.LEFT:
 			"""
 				If statment is needed so that the position doesn't 
-					still go left while the camera is not.
+					still move in a direction while the camera is not.
 			"""
-			if not (position <= Vector2(limit_left,0)): 
-				position -= Vector2(speed * delta, 0)
+			#if not (position <= Vector2(limit_left,0)): 
+			position -= Vector2(speed * delta, 0)
 		CameraMotion.RIGHT:
-			if not (position >= Vector2(limit_right, 0)):
-				position += Vector2(speed * delta, 0)
+			#if not (position >= Vector2(limit_right, 0)):
+			position += Vector2(speed * delta, 0)
 		CameraMotion.UP:
-			if not (position <= Vector2(0, limit_top)): 
-				position -= Vector2(0, speed * delta)
+			#if not (position <= Vector2(0, limit_top)): 
+			position -= Vector2(0, speed * delta)
 		CameraMotion.DOWN:
-			if not (position >= Vector2(0, limit_bottom)): 
-				position += Vector2(0, speed * delta)
+			#if not (position >= Vector2(0, limit_bottom)):
+			position += Vector2(0, speed * delta)
 	multiply_speed()
 
 func _on_timer_timeout() -> void:
